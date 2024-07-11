1ae4a73540444e4ea251425b78cd5c542ae87674
Dima228-ux <61840336+Dima228-ux@users.noreply.github.com>
2024-07-11 15:07:18 +0300

    #admin #add categories & equipments pages for

---
+++ source-code/app/Http/Requests/EquipmentsRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class EquipmentsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=> [
+                'required',
+                'min:2',
+                'max:250',
+                Rule::unique('equipments', 'name')->ignore(request()->id)
+            ],
+            'description' => [
+                'required',
+                'string',
+                'min:2',
+            ],
+            'category_id' =>[
+                'required',
+            ],
+            //'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ];
+    }
+}
